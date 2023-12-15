97553346fb873edc7198de6f544cdbbc6e05bcd3
Wahandri <wahandricode@gmail.com>
2023-12-15 10:31:19 +0100

    Change in styles: character card

--- src/components/RickAndMortyApp/Characters/Characters.js
+++ src/components/RickAndMortyApp/Characters/Characters.js
@@ -1,7 +1,7 @@
 import male from "../../../images/masculino.png";
 import female from "../../../images/femenino.png";
-import live from "../../../images/latido-del-corazon.png";
-import dead from "../../../images/craneo.png";
+import live from "../../../images/circleGreen.png";
+import dead from "../../../images/circleRed.png";
 import statusUnknown from "../../../images/pregunta.png";
 import "./Characters.css";
 
@@ -10,37 +10,38 @@ export default function Characters({ characters = [] }) {
     <div className="bodyCharacters ">
       {characters.map((item, index) => (
         <div className="boxCharacters" key={index}>
-          
-          <div className="cardCharacter bg-myColor">
-            <img className="imgCharacter" src={item.image} alt={item.name} />
+          <div className="boxImage">
+            <img className="imgCharacter" src={item.image} alt="" />
+          </div>
+          <div className="paddingColor">
             <div className="boxDescription ">
-              <h2 className="myColor1">{item.name}</h2>
+              <h4 className="myColor1">{item.name}</h4>
               <hr className="hr" />
-              <h4 className="">
+              <h6 className="">
                 Species:{" "}
                 {item.species === "unknown" ? (
                   <img width="20px" src={statusUnknown} alt="" />
                 ) : (
                   item.species
                 )}
-              </h4>
-              <h4 className="">
+              </h6>
+              <h6 className="">
                 Gender:{" "}
                 {item.gender === "Male" ? (
                   <img width="20px" src={male} alt="" />
                 ) : (
                   <img width="20px" src={female} alt="" />
                 )}
-              </h4>
-              <h4 className="">
+              </h6>
+              <h6 className="">
                 Origin:{" "}
                 {item.origin.name === "unknown" ? (
                   <img width="20px" src={statusUnknown} alt="" />
                 ) : (
                   item.origin.name
                 )}
-              </h4>
-              <h4 className="">
+              </h6>
+              <h6 className="">
                 Status:{" "}
                 {item.status === "Alive" ? (
                   <img width="20px" src={live} alt="" />
@@ -49,7 +50,7 @@ export default function Characters({ characters = [] }) {
                 ) : (
                   <img width="20px" src={statusUnknown} alt="" />
                 )}
-              </h4>
+              </h6>
             </div>
           </div>
         </div>
